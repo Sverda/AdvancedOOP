89c5c0d6b782080b62997a9dfce41d5c88f0ff66
Sverda <sverda300@gmail.com>
2017-11-02 19:42:23 +0100

    Insert random elements into stock

--- AdvancedOOP/Main.cpp
+++ AdvancedOOP/Main.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
+#include<stdlib.h>
+#include <ctime>
+#include "Content.h"
 using namespace std;
 
-void Main()
+void main()
 {
+	Content<double> container;
 
+	srand(time(nullptr));
+	int stockCount = rand() % 100;
+	//cout << "Number of elements on stock: " << stockCount << endl;
+	for (int i = 0; i < stockCount; i++)
+	{
+		double randomNumber = (double)rand() / (double)RAND_MAX * 10.0;
+		container.push(randomNumber);
+	}
+	/*for (int i = 0; i < stockCount; ++i)
+	{
+		cout << "Element " << i << ": " << container.pop() << endl;
+	}*/
+	getchar();
 }
