cd432475c42eb7c7075e009a3c26c3870e9045db
Sverda <sverda300@gmail.com>
2017-11-12 09:55:21 +0100

    Implement content.h

--- AdvancedOOP/Main.cpp
+++ AdvancedOOP/Main.cpp
@@ -9,14 +9,14 @@ void main()
 	Content<double> container;
 
 	srand(time(nullptr));
-	int stockCount = rand() % 100;
-	//cout << "Number of elements on stock: " << stockCount << endl;
-	for (int i = 0; i < stockCount; i++)
+	int stackCount = rand() % 100;
+	//cout << "Number of elements on stock: " << stackCount << endl;
+	for (int i = 0; i < stackCount; i++)
 	{
 		double randomNumber = (double)rand() / (double)RAND_MAX * 10.0;
 		container.push(randomNumber);
 	}
-	/*for (int i = 0; i < stockCount; ++i)
+	/*for (int i = 0; i < stackCount; ++i)
 	{
 		cout << "Element " << i << ": " << container.pop() << endl;
 	}*/
