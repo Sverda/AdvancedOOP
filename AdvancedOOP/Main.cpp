a2b60e7ba8e578707820c941ba87ade156f6d67a
Sverda <sverda300@gmail.com>
2018-01-02 10:10:19 +0100

    Initialize data

--- AdvancedOOP/Main.cpp
+++ AdvancedOOP/Main.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
+#include<list>
+#include<set>
+#include<time.h>
 using namespace std;
+
+multiset<int> InitializeObservations();
 
 void Main()
 {
+	multiset<int> r = InitializeObservations();
+	multiset<int> s = InitializeObservations();
+}
 
+multiset<int> InitializeObservations()
+{
+	srand(time(NULL));
+	multiset<int> observations;
+	for (int i = 0; i<1000; i++)
+		observations.insert(rand() % 10);
+	return observations;
 }
